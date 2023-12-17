86ef405e7fce23f82407b7822c8a19f482993923
rymdhund <7973243+rymdhund@users.noreply.github.com>
2023-12-17 12:23:00 +0100

    Day 17

---
+++ src/bin/day17.rs
@@ -0,0 +1,70 @@
+use priority_queue::DoublePriorityQueue;
+
+use aoc2023::coord::{Coord, CoordMap, Dir};
+
+fn parse(file: &str) -> Vec<Vec<u8>> {
+    std::fs::read_to_string(file).unwrap().trim().lines().map(|line| {
+        line.chars().map(|c| c.to_digit(10).unwrap().try_into().unwrap()).collect()
+    }).collect()
+}
+
+fn astar(map: &Vec<Vec<u8>>, min_steps: usize, max_steps: usize) -> usize {
+    let width = map.len();
+    let start = Coord::new(0, 0);
+    let goal = Coord::new(width as i32 -1, width as i32 -1);
+    let mut open = DoublePriorityQueue::new();
+    open.push((start, Dir::Right), 0);
+    open.push((start, Dir::Down), 0);
+
+    // one cost for each direction we could be facing
+    let mut costs = vec![vec![[usize::MAX; 4]; width]; width];
+    costs[0][0][Dir::Right.id()] = 0;
+    costs[0][0][Dir::Down.id()] = 0;
+
+    while !open.is_empty() {
+        let ((pos, dir), s) = open.pop_min().unwrap();
+
+        if pos == goal{
+            return s;
+        }
+
+        for ((p2, d2), c) in neighbours(map, min_steps, max_steps, pos, dir) {
+            let new_cost = c + costs.at(pos)[dir.id()];
+            if new_cost < costs.at(p2)[d2.id()] {
+                costs[p2.y_u()][p2.x_u()][d2.id()] = new_cost;
+                let score = new_cost + (goal - p2).manhattan();
+                open.push((p2, d2), score);
+            }
+        }
+    }
+    panic!("unexpected");
+}
+
+fn neighbours(map: &Vec<Vec<u8>>, min_steps: usize, max_steps: usize, pos: Coord, dir: Dir) -> Vec<((Coord, Dir), usize)> {
+    let mut res = vec![];
+    let step = Coord::dir(dir);
+
+    let mut p2 = pos;
+    let mut cost = 0;
+    for n in 1..=max_steps {
+        p2 = p2 + step;
+        if !map.contains(p2) {
+            break;
+        }
+        cost += *map.at(p2) as usize;
+        if n >= min_steps {
+            res.push(((p2, dir.turnLeft()), cost));
+            res.push(((p2, dir.turnRight()), cost));
+        }
+    }
+    res
+}
+
+fn main() {
+    let ex = parse("inputs/day17_ex.txt");
+    let inp = parse("inputs/day17.txt");
+    assert!(astar(&ex, 0, 3) == 102);
+    assert!(astar(&ex, 4, 10) == 94);
+    println!("sol1: {}", astar(&inp, 0, 3));
+    println!("sol2: {}", astar(&inp, 4, 10));
+}
---
+++ src/lib.rs
@@ -0,0 +1,130 @@
+pub mod coord {
+    use std::ops::{Add, Sub};
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct Coord {
+        x: i32,
+        y: i32,
+    }
+
+    impl Coord {
+        pub fn new(x: i32, y: i32) -> Self {
+            Coord {x, y}
+        }
+
+        pub fn up() -> Coord {
+            Coord::new(0, -1)
+        }
+
+        pub fn right() -> Coord {
+            Coord::new(1, 0)
+        }
+
+        pub fn down() -> Coord {
+            Coord::new(0, 1)
+        }
+
+        pub fn left() -> Coord {
+            Coord::new(-1, 0)
+        }
+
+        pub fn dir(d: Dir) -> Coord {
+            match d {
+                Dir::Right => Self::right(),
+                Dir::Down => Self::down(),
+                Dir::Left => Self::left(),
+                Dir::Up => Self::up(),
+            }
+        }
+
+        pub fn x_u(&self) -> usize {
+            self.x.try_into().unwrap()
+        }
+
+        pub fn y_u(&self) -> usize {
+            self.y.try_into().unwrap()
+        }
+
+        pub fn manhattan(&self) -> usize {
+            (self.x.abs() + self.y.abs()).try_into().unwrap()
+        }
+    }
+
+    impl Add for Coord {
+        type Output = Self;
+
+        fn add(self, rhs: Self) -> Self::Output {
+            Coord::new(self.x + rhs.x, self.y + rhs.y)
+        }
+    }
+
+    impl Sub for Coord {
+        type Output = Self;
+
+        fn sub(self, rhs: Self) -> Self::Output {
+            Coord::new(self.x - rhs.x, self.y - rhs.y)
+        }
+    }
+
+    #[derive(Hash, PartialEq, Eq, Clone, Copy)]
+    pub enum Dir {
+        Right,
+        Down,
+        Left,
+        Up,
+    }
+
+    impl Dir {
+        pub fn of_id(id: usize) -> Dir {
+            match id {
+                0 => Dir::Right,
+                1 => Dir::Down,
+                2 => Dir::Left,
+                3 => Dir::Up,
+                _ => panic!("Invalid direction id: {id}"),
+            }
+        }
+
+        pub fn id(self) -> usize {
+            match self {
+                Dir::Right => 0,
+                Dir::Down => 1,
+                Dir::Left => 2,
+                Dir::Up => 3,
+            }
+        }
+
+        pub fn turnLeft(self) -> Dir {
+            Self::of_id((self.id() + 3) % 4)
+        }
+
+        pub fn turnRight(self) -> Dir {
+            Self::of_id((self.id() + 1) % 4)
+        }
+    }
+
+    pub trait CoordMap<T> {
+        fn at(&self, p: Coord) -> &T;
+        fn contains(&self, p: Coord) -> bool;
+        fn print(&self);
+    }
+
+    impl<T> CoordMap<T> for Vec<Vec<T>> where T: std::fmt::Debug {
+        fn at(&self, p: Coord) -> &T {
+            let y: usize = p.y.try_into().unwrap();
+            let x: usize = p.x.try_into().unwrap();
+            &self[y][x]
+        }
+
+        fn contains(&self, p: Coord) -> bool {
+            p.x >= 0 && p.y >= 0 && p.x < self[0].len().try_into().unwrap() && p.y < self.len().try_into().unwrap()
+        }
+
+        fn print(&self) {
+            for row in self {
+                row.iter().for_each(|x| print!("{x:?}"));
+                println!("");
+            }
+        }
+    }
+}
